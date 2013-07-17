f9b830e419860f2ed7d721381e1af382ca396192
Taylor Donner <atdonner@gmail.com>
2013-07-17 15:30:31 -0500

    Completed jquery fadeIn fadeOut of image thumbnails

--- imagilizer.js
+++ imagilizer.js
@@ -1,21 +1,37 @@
+function display_photo(element){
+	var cloned_element = element.clone();
+	cloned_element
+	.appendTo('#panel')
+	.removeAttr('width')
+	.addClass('active');
+	return cloned_element;
+}
+
+function change_thumbnail_class(element){
+	if(element.hasClass('active')){
+		} else {
+			element.siblings().removeClass('active');
+			element.addClass('active');
+		}
+}
 
 $(document).ready(function(){
 	// Show the first thumbnail image and append it to the panel
 	$('img:first').toggleClass('active');
-	var first_img = $('img:first').clone().appendTo('#panel').removeAttr('width').removeClass('active');
+	display_photo($('#sidebar img:first'));
 	// Changing the status and showing the thumbnail in the panel when clicked.
 	$('#sidebar img').click(function(){
 		var selected_photo = $(this);
 		// Change active status of the thumbnails
-		if($(this).hasClass('active')){
-		} else {
-			$(this).siblings().removeClass('active');
-			$(this).addClass('active');
-		}
+
+		change_thumbnail_class($(this));
+	
 		// Change panel photo when thumbnail is clicked
-		$('#panel img').fadeOut(function(){
-			selected_photo.clone().appendTo('#panel').removeAttr('width').removeClass('active');
-			$(this).remove();
-		});
+		$('#panel img').fadeOut('slow');
+
+		display_photo(selected_photo).hide().fadeIn('slow');
+		$('#panel img:first').remove();
+			
+			
 	});			
 });
