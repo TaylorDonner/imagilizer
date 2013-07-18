2bfe237daba1e001ecdd6475e8b67c4d54569adf
Taylor Donner <atdonner@gmail.com>
2013-07-17 20:28:11 -0500

    refactored function names, still need to add two more functions

--- imagilizer.js
+++ imagilizer.js
@@ -1,37 +1,38 @@
 $(document).ready(function(){
 	var caption = $('.caption');
 	var sidebarImages = $('#sidebar img');
+	
 	// Show the first thumbnail image and append it to the panel
 	$('img:first').toggleClass('active');
-	display_photo(sidebarImages.first());
+	displayPhoto(sidebarImages.first());
 	// This needs to be refactored, but I display data-caption on page load
-	caption.text(display_photo(sidebarImages.first()).attr('data-caption'));
+	caption.text(displayPhoto(sidebarImages.first()).attr('data-caption'));
 	// Changing the status and showing the thumbnail in the panel when clicked.
 	sidebarImages.click(function(){
-		var selected_photo = $(this);
+		var panelImg = $('#panel img');
+		var selectedPhoto = $(this);
 		// Change active status of the thumbnails
-		change_thumbnail_class($(this));
+		changeThumbnailClass($(this));
 		// Change panel photo and add caption to the top of the panel when thumbnail is clicked 
-		$('#panel img').fadeOut('slow');
-		caption.text(display_photo(selected_photo).hide().fadeIn('slow').attr('data-caption'));
-		$('#panel img:first').remove();		
+		panelImg.fadeOut('slow');
+		caption.text(displayPhoto(selectedPhoto).hide().fadeIn('slow').attr('data-caption'));
+		panelImg.first().remove();		
 	});			
 });
 
-function display_photo(element){
-	var cloned_element = element.clone();
-	cloned_element
+function displayPhoto(element){
+	var clonedElement = element.clone();
+	clonedElement
 	.appendTo('#panel')
 	.removeAttr('width')
 	.addClass('active');
-	return cloned_element;
+	return clonedElement;
 }
 
-function change_thumbnail_class(element){
+function changeThumbnailClass(element){
 	if(element.hasClass('active')){
 		} else {
 			element.siblings().removeClass('active');
 			element.addClass('active');
 		}
 }
-// function show_caption(display_photo(element))
